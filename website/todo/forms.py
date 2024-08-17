fb1967eb74f101ffaeb80ab40ed779b4538f550c
KianMehrabi <gmkianmehrabi@gmail.com>
2024-08-17 14:08:45 +0330

    adding login logic form

--- website/todo/forms.py
+++ website/todo/forms.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm , AuthenticationForm
 from django.contrib.auth.models import User
 
 from django import forms
-
+from django.forms.widgets import PasswordInput , TextInput
 
 class CreateUserForm(UserCreationForm):
     
@@ -10,3 +10,6 @@ class CreateUserForm(UserCreationForm):
         models = User
         fields = ['username' , 'email' ,  'password1' , 'password2'] 
         
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
