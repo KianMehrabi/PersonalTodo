1f5f18c6f600e72121d26a236f911fd2af4fbed3
KianMehrabi <gmkianmehrabi@gmail.com>
2024-08-17 12:32:01 +0330

    adding the auth forms for user creation(signup logic)

---
+++ website/todo/forms.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from django import forms
+
+
+class CreateUserForm(UserCreationForm):
+    
+    class Meta:
+        models = User
+        fields = ['username' , 'email' ,  'password1' , 'password2'] 
+        
