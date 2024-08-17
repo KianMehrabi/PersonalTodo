676941f65c5219ce0647447ce6ad0db492a952e0
KianMehrabi <gmkianmehrabi@gmail.com>
2024-08-17 14:21:28 +0330

    adding the login logic to the views.py

--- website/todo/views.py
+++ website/todo/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render , redirect
 
 # importing logic from forms
-from .forms import UserCreationForm
+from .forms import *
+
+# for login stuff
 from django.contrib.auth import authenticate, login
 
 def home(request):
@@ -17,7 +19,24 @@ def sign_up(request):
                                     password=form.cleaned_data['password1'],
                                     )
             login(request, new_user)
-            return redirect("hello_world/")
+            return redirect("home")
     else:
         form = UserCreationForm()
     return render(request , "signup.html" , context = {'sign_up_form': UserCreationForm} )
+
+def login(request):
+    form = LoginForm() 
+    if request.method == "POST":
+        form = LoginForm(request , data = request.POST)
+        
+        if form.is_valid:
+            username = request.POST.get('username')
+            password = request.POST.get('password') 
+            user = authenticate(request , username = username , password = password)
+            
+            if user is not None:
+                login(request , user)
+                
+                return redirect("home")
+    context = {'login_form': LoginForm}
+    return render(request , "login.html" , context = context )
